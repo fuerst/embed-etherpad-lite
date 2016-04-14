982ecdbf78f9037e740a2f6ab825975a6996c03d
Bernhard Fürst <bernhard.fuerst@fuerstnet.de>
2016-04-14 21:27:00 +0200

    Add plugin code.

---
+++ plugin.rb
@@ -0,0 +1,16 @@
+# name: embed-etherpad-lite
+# about: Embed Etherpad Lite pads using Onebox.
+# version: 1.0
+# authors: Bernhard Fürst
+# url: https://dev.fuerstnet.de/discourse-onebox/embed-etherpad-lite
+
+class Onebox::Engine::EtherpadLiteOnebox
+  include Onebox::Engine
+  # Try to guess Etherpad Lite links like http(s)://domain.tld/p/pad-id
+  matches_regexp /^https?:\/\/[^\/]+\/p\/[^\/]+/
+
+  # See https://github.com/ether/etherpad-lite/wiki/Embed-Parameters
+  def to_html
+    "<iframe name='embed_readwrite' src='#{@url}?showControls=true&showChat=true&showLineNumbers=true&useMonospaceFont=false' style='border-width:0' frameborder='0' scrolling='no' width='100%' height=600></iframe><br><a href='#{@url}' target='_blank'>Goto this EtherPad</a>"
+  end
+end
